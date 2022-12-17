cb5b296afbb75fb55f45468d492172368290ceb0
vudala <vudalae@gmail.com>
2022-12-17 13:40:12 -0300

    modularizando o playfair

---
+++ playfair.h
@@ -0,0 +1,11 @@
+#ifndef PLAYFAIR_INCLUDED
+#define PLAYFAIR_INCLUDED
+
+#include <bits/stdc++.h>
+
+std::string playfair_encrypt(std::string key, std::string text);
+
+std::string playfair_decrypt(std::string key, std::string text);
+
+
+#endif
